2440f6a9cac17bfd77e03c676f37c2bf3015380b
admin <sapozhnikoff1996@gmail.com>
2024-02-12 02:02:30 +0700

    stream

---
+++ src/main/java/ru/topics/stream_api/StreamAPIProcessor.java
@@ -0,0 +1,58 @@
+package ru.topics.stream_api;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamAPIProcessor {
+
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("one","two","three","four","five");
+        List<Integer> integers = Arrays.asList(5,5,51,2,3,4,5,5,5,5,5);
+        Map<Integer,String> map = Map.of(1, "1", 2, "2", 3, "3", 4, "5");
+        String[] stringArray = new String[]{"Winter", "Spring", "Summer", "Autumn"};
+
+        /**
+         * Пустой стрим
+         */
+        Stream.empty();
+
+        /**
+         * Стрим из List
+         */
+        strings.stream();
+
+        /**
+         * Стрим из Map
+         */
+        map.entrySet().stream();
+
+        /**
+         * Стрим из массива
+         */
+        Arrays.stream(stringArray);
+
+        /**
+         * Стрим из элементов
+         */
+        Stream.of(1,2,3);
+
+        /**
+         * Пример использования 2ух промежуточных операторов filter() и map() и терминального collect()
+         */
+        integers.stream()
+                .filter(x -> x > 2)
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+
+        /**
+         * Пример использования 2ух промежуточных операторов distinct() и sorted() и терминального forEach()
+         */
+        integers.stream()
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
+    }
+}
