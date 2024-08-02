8f85cd8a105b9df87643aae3685abae3ef0f491d
admin <sapozhnikoff1996@gmail.com>
2024-08-02 11:00:59 +0300

    just some upd

--- src/main/java/ru/topics/final_word/FinalClass.java
+++ src/main/java/ru/topics/final_word/FinalClass.java
@@ -1,3 +1,26 @@
 package ru.topics.final_word;
 
-public final class FinalClass {}
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+public final class FinalClass {
+
+    private String greetings;
+    private String name;
+
+    public static void main(String[] args) {
+        FinalClass finalClass = new FinalClass()
+                .setName("Христичка")
+                .setGreetings("Привет");
+        System.out.println(finalClass.toString());
+    }
+
+    @Override
+    public String toString() {
+        return greetings + ", " + name;
+    }
+}
---
+++ src/main/java/ru/topics/servlets/MyServlet.java
@@ -0,0 +1,22 @@
+package ru.topics.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter pw = resp.getWriter();
+        pw.write("Прив");
+        pw.close();
+        req.getServletContext();
+    }
+}
